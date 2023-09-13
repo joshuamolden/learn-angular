d498b184e74bda4f9e232ec3fcc193ef344352ac
Joshua Molden <jmolden@bushelpowered.com>
2023-09-13 21:13:01 +0000

    Merged PR 238: Part 2: Add selection list
    Part 2: Add selection list
    
    Here is the angular tutorial link for the work done in this PR: https://angular.io/tutorial/tour-of-heroes/toh-pt2
    
    Here are my notes regarding the work I did: https://docs.google.com/document/d/1k3D2-4XZoJz8wTG52bbNq6jM8oxJ0kYaulmmPx2NYO0/edit#heading=h.vs7akrn2nvq4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { Hero } from '../hero';
+
+import { HEROES } from 'app/mock-heroes';
+import { Hero } from 'app/hero';
 
 @Component({
   selector: 'app-heroes',
@@ -7,5 +9,9 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  hero: Hero = { id: 1, name: 'Windstorm' };
+  heroes: Hero[] = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
