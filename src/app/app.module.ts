50d25a9cec10630c3766fe77e8c653880afc552c
Joshua Molden <jmolden@bushelpowered.com>
2023-09-13 22:05:27 +0000

    Merged PR 240: Part 3: Creating a Feature Component
    Part 3: Creating a Feature Component
    
    Here is the angular tutorial link for the work done in this PR: https://angular.io/tutorial/tour-of-heroes/toh-pt3
    
    Here are my notes regarding the work I did: https://docs.google.com/document/d/1k3D2-4XZoJz8wTG52bbNq6jM8oxJ0kYaulmmPx2NYO0/edit#heading=h.hrgnchrr6x67

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from 'app/app-routing.module';
 import { AppComponent } from 'app/app.component';
 import { HeroesComponent } from 'app/heroes/heroes.component';
+import { HeroDetailComponent } from 'app/hero-detail/hero-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
